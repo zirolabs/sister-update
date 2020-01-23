221e4259b6f3fc8249a21e3de102dcb30fe2cda5
fendi <tusfendi@gmail.com>
2020-01-24 05:06:32 +0700

    Menambahkan model filter semua guru

--- modules/manajemen_guru/models/Manajemen_guru_model.php
+++ modules/manajemen_guru/models/Manajemen_guru_model.php
@@ -2,6 +2,7 @@
 
 class Manajemen_guru_model extends CI_Model
 {
+	// menampilkan semua wali kelas berdasakan filter 
 	function get_data($param = array())
 	{
 		$level_user 	= $this->session->userdata('login_level');
@@ -83,7 +84,81 @@ class Manajemen_guru_model extends CI_Model
 		return $get;
 	}
 
-	// Modal untuk mendapatkan data semua user dengan level guru
+	// menampilkan semua guru berdasakan filter
+	function get_data_guru($param = array())
+	{
+		$level_user 	= $this->session->userdata('login_level');
+		$id_user 	 	= $this->session->userdata('login_uid');
+
+		if(!empty($param))
+		{
+			if(!empty($param['limit']))
+			{
+				if(!empty($param['offset']))
+				{
+					$this->db->limit($param['limit'], $param['offset']);
+				}
+				else
+				{
+					$this->db->limit($param['limit']);
+				}
+			}
+
+			if(!empty($param['keyword']))
+			{
+				$this->db->like('a.nama', $param['keyword']);
+			}
+
+			if(!empty($param['sekolah']))
+			{
+				$this->db->where('b.sekolah_id', $param['sekolah']);
+			}
+
+			if(!empty($param['kepala sekolah']))
+			{
+				$level_user = 'kepala sekolah';
+				$id_user 	= $param['kepala sekolah'];
+			}
+
+			if(!empty($param['operator sekolah']))
+			{
+				$level_user = 'operator sekolah';
+				$id_user 	= $param['operator sekolah'];
+			}
+
+			if(!empty($param['guru']))
+			{
+				$level_user = 'guru';
+				$id_user 	= $param['guru'];
+			}	 			
+		}
+
+		$this->db->select("
+			a.*, 
+			b.*, 
+			c.nama as sekolah
+		");
+		$this->db->order_by('a.nama');
+		$this->db->from('user a');
+		$this->db->join('user_guru b', 'b.user_id = a.user_id');
+		$this->db->join('profil_sekolah c', 'b.sekolah_id = c.sekolah_id');
+
+		if($level_user == 'kepala sekolah')
+		{
+			$this->db->where('x.user_id', $id_user);
+			$this->db->join('user_kepala_sekolah x', 'x.sekolah_id = e.sekolah_id');
+		}
+		elseif($level_user == 'operator sekolah')
+		{
+			$this->db->where('x.user_id', $id_user);
+			$this->db->join('user_operator x', 'x.sekolah_id = c.sekolah_id');
+		}
+
+		$get = $this->db->get();
+		return $get;
+	}
+
+	// Modal untuk mendapatkan data semua user dengan level guru berdasarkan sekolah_id
 	function get_data_all($param = array())
 	{	
 		$this->db->select("
