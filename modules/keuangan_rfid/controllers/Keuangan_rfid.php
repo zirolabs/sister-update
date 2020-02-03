6cb9d666e90f60914942180e55edcacdf0548a24
fendi <tusfendi@gmail.com>
2020-02-04 06:04:27 +0700

    fix bug pendaftaran rfid & qrcode

--- modules/keuangan_rfid/controllers/Keuangan_rfid.php
+++ modules/keuangan_rfid/controllers/Keuangan_rfid.php
@@ -112,7 +112,7 @@ class Keuangan_rfid extends CI_Controller
 					$this->session->set_flashdata('msg', info_msg('Kartu RFID sudah terdaftar dengan siswa yang bersangkutan.'));
 				}
 				else
-				{	
+				{
 					$this->session->set_flashdata('msg', err_msg('Kartu RFID sudah terdaftar pada siswa lain, gunakan kartu lain.'));
 				}
 
