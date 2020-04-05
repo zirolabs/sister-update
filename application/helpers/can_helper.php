00ac14b772e86a83be7916022ba002d04ab29ffa
tusfendi <tusfendi@gmail.com>
2020-04-05 11:11:42 +0700

    Helper : penambahan fungsi tanggal indonesia

--- application/helpers/can_helper.php
+++ application/helpers/can_helper.php
@@ -132,6 +132,16 @@ function hari_indonesia($index = '')
 	return $list[$index];
 }
 
+function day($index = '')
+{
+	$list = array('Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat');
+	if($index == '')
+	{
+		return $list;
+	}
+	return $list[$index];
+}
+
 function bulan_indonesia($index = '')
 {
 	$list = array(
@@ -370,3 +380,20 @@ function waktu_berlalu($datetime = '', $full = false)
     }
     return $string ? implode(', ', $string) . ' yang lalu' : 'baru saja';
 }
+
+function tanggal_indo($date)
+{
+	/* ARRAY u/ hari dan bulan */
+	$Hari = array ("Minggu", "Senin", "Selasa", "Rabu", "Kamis", "Jumat", "Sabtu",);
+	$Bulan = array ("Januari", "Februari", "Maret", "April", "Mei", "Juni", "Juli", "Agustus", "September", "Oktober", "November", "Desember");
+ 
+	/* Memisahkan format tanggal bulan dan tahun menggunakan substring */
+	$tahun 	 = substr($date, 0, 4);
+	$bulan 	 = substr($date, 5, 2);
+	$tgl	 = substr($date, 8, 2);
+	// $waktu	 = substr($date,11, 5);
+	$hari	 = date("w", strtotime($date));
+	
+	$result = $Hari[$hari].", ".$tgl." ".$Bulan[(int)$bulan-1]." ".$tahun;
+	return $result;
+ }
